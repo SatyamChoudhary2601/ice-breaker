81011b5510b90dc98628afce7bdb325048bd50b3
= <=>
2020-12-23 09:54:10 +0530

    new

--- src/App.js
+++ src/App.js
@@ -8,16 +8,46 @@ const initialState = {
   alertShow:true
 }
 
+
+
 function App() {
-  const [alert, setAlert] = useState(initialState)
+  const [alert, setAlert] = useState(initialState);
+  // const [counter, setCounter] = useState(new Date().getSeconds())
+  // console.log(counter)
+  
+  let newSecond = new Date().getSeconds()
+  // let i =0;
+  // let videoNumber = 0
+  // while(i<=13){
+  //   console.log("pause")
+  //   if(i == 12 && videoNumber < 5){
+  //       console.log("i"+i+"video"+ videoNumber);
+  // }
+  
+  
+  // let i = 0;
+
+  // let videoNumber = 0
+  // while(i<=13){
+  //   // console.log("stated");
+  //   // counter++
+  //   if(i == 12 && videoNumber < 5){
+  //   console.log("i"+i+"video"+ videoNumber);
+      
+  //     i=0
+  //     videoNumber = +1
+      
+  //   }
+  // }
 
   const volumeControl = () => {
     setAlert({alertShow:false})
   }
+
   useEffect(()=>{
     setTimeout(() => {
       volumeControl()
-    }, 6000)
+    }, 1200)
   }
     ,[alert])
   return (
@@ -25,6 +55,8 @@ function App() {
       {alert.alertShow? <Alert/>:null}
       <VideoComponent />
       <AudioComponent/>
+      {/* Counter: {counter} */}
+      Time: {newSecond}
     </div>
   );
 }
